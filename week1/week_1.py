1973ee04a070f261cc606b5da339e5e554fe8e5b
haroon835 <56167313+haroon835@users.noreply.github.com>
2019-10-28 13:00:07 +0000

    Update week_1.py

--- week1/week_1.py
+++ week1/week_1.py
@@ -1,1 +1,12 @@
 
+radius = float
+radius = input("Enter the radius of circle")
+
+pi = 22 / 7
+
+radius2 = float
+radius2 = float(radius) * float(radius)
+
+area = float
+area = float(pi) * float(radius2)
+print(round(area, 2),"cm")
